41d8711caefffe8fe85861e5571bcb773a328120
FastBack2499 <64218658+FastBack2499@users.noreply.github.com>
2024-01-26 18:31:24 +0330

    Add files via upload

---
+++ src/components/index.js
@@ -0,0 +1,29 @@
+import Navbar from "./Navbar";
+import Billing from "./Billing";
+import Business from "./Business";
+import Button from "./Button";
+import Clients from "./Clients";
+import Stats from "./Stats";
+import Footer from "./Footer"
+import Testimonials from "./Testimonials";
+import Hero from "./Hero";
+import CardDeal from "./CardDeal";
+import CTA from "./CTA";
+import GetStarted from "./GetStarted";
+import FeedbackCard from "./FeedbackCard";
+
+export {
+    Navbar,
+    Billing,
+    Business,
+    Button,
+    Clients,
+    Stats,
+    Footer,
+    Testimonials,
+    Hero,
+    CTA,
+    GetStarted,
+    CardDeal,
+    FeedbackCard,
+};
